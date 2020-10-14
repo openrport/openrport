046655db93c463107ab7946247c53a4d7b8cc82b
Mykola Terelia <69863527+m-terel@users.noreply.github.com>
2020-10-14 18:12:12 +0300

    DEV-1646: Fix imports (#71)

--- client/exec_win.go
+++ client/exec_win.go
@@ -2,7 +2,12 @@
 
 package chclient
 
-import "errors"
+import (
+	"context"
+	"errors"
+
+	"github.com/cloudradar-monitoring/rport/share/comm"
+)
 
 func (c *Client) HandleRunCmdRequest(ctx context.Context, reqPayload []byte) (*comm.RunCmdResponse, error) {
 	return nil, errors.New("remote command execution not supported")
