f6feab15a8f245d71e56c61c349ab5581b510bc5
Andrey Pozolotin <posandrey@gmail.com>
2021-09-15 14:43:08 +0300

    DEV-2235 Refactored mocked fields

--- server/script/executor_test.go
+++ server/script/executor_test.go
@@ -20,57 +20,57 @@ func TestCreateScriptOnClient(t *testing.T) {
 	const defaultHash = "a1159e9df3670d549d04524532629f5477ceb7deec9b45e47e8c009506ecb2c8" //sha256 of "pwd"
 	const defaultScript = "pwd"
 	testCases := []struct {
-		name                  string
-		filePathToReturn      string
-		Sha256HashToReturn    string
-		clientOSKernelToGive  string
-		scriptToGive          string
-		fileExtensionToExpect string
-		interpreterToGive     string
+		name               string
+		filePathWant         string
+		Sha256HashToMocked   string
+		clientOSKernelMocked string
+		scriptMocked         string
+		fileExtensionWant string
+		interpreterMocked string
 	}{
 		{
-			name:                  "sh script linux",
-			filePathToReturn:      "/tmp/script.sh",
-			Sha256HashToReturn:    defaultHash,
-			clientOSKernelToGive:  "linux",
-			scriptToGive:          defaultScript,
-			fileExtensionToExpect: ".sh",
+			name:                 "sh script linux",
+			filePathWant:         "/tmp/script.sh",
+			Sha256HashToMocked:   defaultHash,
+			clientOSKernelMocked: "linux",
+			scriptMocked:         defaultScript,
+			fileExtensionWant:    ".sh",
 		},
 		{
-			name:                  "taco script linux",
-			interpreterToGive:     chshare.Taco,
-			filePathToReturn:      "/tmp/taco_script.yml",
-			Sha256HashToReturn:    defaultHash,
-			clientOSKernelToGive:  "linux",
-			scriptToGive:          defaultScript,
-			fileExtensionToExpect: ".yml",
+			name:                 "taco script linux",
+			interpreterMocked:    chshare.Taco,
+			filePathWant:         "/tmp/taco_script.yml",
+			Sha256HashToMocked:   defaultHash,
+			clientOSKernelMocked: "linux",
+			scriptMocked:         defaultScript,
+			fileExtensionWant:    ".yml",
 		},
 		{
-			name:                  "taco script windows",
-			interpreterToGive:     chshare.Taco,
-			filePathToReturn:      "C:\\taco_script.yml",
-			Sha256HashToReturn:    defaultHash,
-			clientOSKernelToGive:  "windows",
-			scriptToGive:          defaultScript,
-			fileExtensionToExpect: ".yml",
+			name:                 "taco script windows",
+			interpreterMocked:    chshare.Taco,
+			filePathWant:         "C:\\taco_script.yml",
+			Sha256HashToMocked:   defaultHash,
+			clientOSKernelMocked: "windows",
+			scriptMocked:         defaultScript,
+			fileExtensionWant:    ".yml",
 		},
 		{
-			name:                  "powershell script windows",
-			interpreterToGive:     chshare.PowerShell,
-			filePathToReturn:      "C:\\ps_script.ps1",
-			Sha256HashToReturn:    defaultHash,
-			clientOSKernelToGive:  "windows",
-			scriptToGive:          defaultScript,
-			fileExtensionToExpect: ".ps1",
+			name:                 "powershell script windows",
+			interpreterMocked:    chshare.PowerShell,
+			filePathWant:         "C:\\ps_script.ps1",
+			Sha256HashToMocked:   defaultHash,
+			clientOSKernelMocked: "windows",
+			scriptMocked:         defaultScript,
+			fileExtensionWant:    ".ps1",
 		},
 		{
-			name:                  "cmd script windows",
-			interpreterToGive:     chshare.CmdShell,
-			filePathToReturn:      "C:\\cmd_script.bat",
-			Sha256HashToReturn:    defaultHash,
-			clientOSKernelToGive:  "windows",
-			scriptToGive:          defaultScript,
-			fileExtensionToExpect: ".bat",
+			name:                 "cmd script windows",
+			interpreterMocked:    chshare.CmdShell,
+			filePathWant:         "C:\\cmd_script.bat",
+			Sha256HashToMocked:   defaultHash,
+			clientOSKernelMocked: "windows",
+			scriptMocked:         defaultScript,
+			fileExtensionWant:    ".bat",
 		},
 	}
 
@@ -78,8 +78,8 @@ func TestCreateScriptOnClient(t *testing.T) {
 		tc := testCases[i]
 		t.Run(tc.name, func(t *testing.T) {
 			givenResp := &comm.CreateFileResponse{
-				FilePath:   tc.filePathToReturn,
-				Sha256Hash: tc.Sha256HashToReturn,
+				FilePath:   tc.filePathWant,
+				Sha256Hash: tc.Sha256HashToMocked,
 			}
 			givenRespBytes, err := json.Marshal(givenResp)
 			require.NoError(t, err)
@@ -91,27 +91,27 @@ func TestCreateScriptOnClient(t *testing.T) {
 			}
 
 			cl := &clients.Client{
-				OSKernel:   tc.clientOSKernelToGive,
+				OSKernel:   tc.clientOSKernelMocked,
 				Connection: conn,
 			}
 			inp := &api.ExecuteInput{
-				Script:      tc.scriptToGive,
-				Interpreter: tc.interpreterToGive,
+				Script:      tc.scriptMocked,
+				Interpreter: tc.interpreterMocked,
 			}
 
-			res, err := executor.CreateScriptOnClient(inp, cl)
+			gotScriptPath, err := executor.CreateScriptOnClient(inp, cl)
 			require.NoError(t, err)
-			assert.Equal(t, tc.filePathToReturn, res)
+			assert.Equal(t, tc.filePathWant, gotScriptPath)
 
-			fileInputGiven := &models.File{}
+			fileInputGot := &models.File{}
 			_, _, fileInputBytes := conn.InputSendRequest()
 
-			err = json.Unmarshal(fileInputBytes, fileInputGiven)
+			err = json.Unmarshal(fileInputBytes, fileInputGot)
 			require.NoError(t, err)
 
-			assert.True(t, strings.HasSuffix(fileInputGiven.Name, tc.fileExtensionToExpect))
-			assert.Equal(t, tc.scriptToGive, string(fileInputGiven.Content))
-			assert.EqualValues(t, DefaultScriptFileMode, fileInputGiven.Mode)
+			assert.True(t, strings.HasSuffix(fileInputGot.Name, tc.fileExtensionWant))
+			assert.Equal(t, tc.scriptMocked, string(fileInputGot.Content))
+			assert.EqualValues(t, DefaultScriptFileMode, fileInputGot.Mode)
 		})
 	}
 }
