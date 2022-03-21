6dc62ea57ec44bf1cbe76d56d32fde693c85abe9
Thorsten Kramm <tk@system42.io>
2022-03-21 16:30:47 +0100

    fixed test

--- share/http_server_test.go
+++ share/http_server_test.go
@@ -1,6 +1,7 @@
 package chshare
 
 import (
+	"crypto/tls"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,7 +16,19 @@ func TestNewHttpServer(t *testing.T) {
 }
 
 func TestNewHttpServerWithTLS(t *testing.T) {
-	s := NewHTTPServer(123, WithTLS("test.crt", "test.key"))
+	tlsConfig := &tls.Config{
+		MinVersion:               tls.VersionTLS13,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+
+	s := NewHTTPServer(123, WithTLS("test.crt", "test.key", tlsConfig))
 
 	assert.Equal(t, 123, s.MaxHeaderBytes)
 	assert.Equal(t, "test.crt", s.certFile)
