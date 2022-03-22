2bf35a07314df51945cef480cb2c53575d818e0a
Thorsten Kramm <tk@system42.io>
2022-03-22 11:30:20 +0100

    Removed all cipher suites because TLSv1.3 selects them automatically

--- share/http_server_test.go
+++ share/http_server_test.go
@@ -20,11 +20,6 @@ func TestNewHttpServerWithTLS(t *testing.T) {
 		MinVersion:               tls.VersionTLS13,
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		},
 	}
 
 	s := NewHTTPServer(123, WithTLS("test.crt", "test.key", tlsConfig))
--- share/security/tls_config.go
+++ share/security/tls_config.go
@@ -6,9 +6,4 @@ var TLSConfig = &tls.Config{
 	MinVersion:               tls.VersionTLS13,
 	CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 	PreferServerCipherSuites: true,
-	CipherSuites: []uint16{
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-	},
 }
