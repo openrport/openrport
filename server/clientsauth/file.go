7b184769fcb99e00fd52622511388a60d9692817
Thorsten Kramm <tk@system42.io>
2022-05-30 16:37:13 +0200

    implementation of filters for client-auth

--- server/clientsauth/file.go
+++ server/clientsauth/file.go
@@ -5,7 +5,14 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/cloudradar-monitoring/rport/share/query"
 
 	"github.com/cloudradar-monitoring/rport/share/enums"
 )
@@ -21,6 +28,24 @@ var _ Provider = &FileProvider{}
 func NewFileProvider(fileName string) *FileProvider {
 	return &FileProvider{
 		fileName: fileName,
+	}
+}
+
+// NewMockFileProvider creates a clients auth file for testing and returns the FileProvider
+func NewMockFileProvider(clients []*ClientAuth, tempDir string) *FileProvider {
+	var authFile = tempDir + "/client-auth.json"
+	f, _ := os.Create(authFile)
+	defer f.Close()
+	clientAuth := make(map[string]string)
+	for _, v := range clients {
+		clientAuth[v.ID] = v.Password
+	}
+	cj, _ := json.Marshal(clientAuth)
+	if _, err := f.Write(cj); err != nil {
+		log.Fatalln("error writing client-auth.json for mock provider", err)
+	}
+	return &FileProvider{
+		fileName: authFile,
 	}
 }
 
@@ -42,13 +67,44 @@ func (c *FileProvider) GetAll() ([]*ClientAuth, error) {
 	return res, nil
 }
 
+func (c *FileProvider) GetFiltered(filter *query.ListOptions) ([]*ClientAuth, int, error) {
+	var ca []*ClientAuth
+	ca, err := c.GetAll()
+	if err != nil {
+		return nil, 0, err
+	}
+	if len(filter.Filters) > 0 {
+		re := regexp.MustCompile("(?i)^" + strings.Replace(filter.Filters[0].Values[0], "*", ".*?", -1) + "$")
+		var filtered = []*ClientAuth{}
+		for _, v := range ca {
+			if re.MatchString(v.ID) {
+				filtered = append(filtered, &ClientAuth{v.ID, v.Password})
+			}
+		}
+		ca = filtered
+	}
+	iLimit, _ := strconv.Atoi(filter.Pagination.Limit)
+	iOffset, _ := strconv.Atoi(filter.Pagination.Offset)
+	c.SortByID(ca, false)
+	l := len(ca)
+	if iLimit > l {
+		iLimit = l
+	}
+	if iOffset > l {
+		iOffset = l
+	}
+	return ca[iOffset:iLimit], l, nil
+}
+
 func (c *FileProvider) Get(id string) (*ClientAuth, error) {
 	idPswdPairs, err := c.load()
 	if err != nil {
 		return nil, fmt.Errorf("failed to decode rport clients auth file: %v", err)
 	}
-
-	return &ClientAuth{ID: id, Password: idPswdPairs[id]}, nil
+	if _, ok := idPswdPairs[id]; ok {
+		return &ClientAuth{ID: id, Password: idPswdPairs[id]}, nil
+	}
+	return nil, nil
 }
 
 func (c *FileProvider) Add(client *ClientAuth) (bool, error) {
@@ -121,4 +177,14 @@ func (c *FileProvider) save(idPswdPairs map[string]string) error {
 
 func (c *FileProvider) Source() enums.ProviderSource {
 	return enums.ProviderSourceFile
+}
+
+func (c *FileProvider) SortByID(a []*ClientAuth, desc bool) {
+	sort.Slice(a, func(i, j int) bool {
+		less := a[i].ID < a[j].ID
+		if desc {
+			return !less
+		}
+		return less
+	})
 }
