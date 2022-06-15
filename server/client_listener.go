c5abec2ddbb6d5a59dfd891c101b9b6a2c68306d
Thorsten Kramm <tk@system42.io>
2022-06-15 11:02:27 +0200

    extended debug logging

--- server/client_listener.go
+++ server/client_listener.go
@@ -216,11 +216,11 @@ func (cl *ClientListener) handleWebsocket(w http.ResponseWriter, req *http.Reque
 	var r *ssh.Request
 	select {
 	case r = <-reqs:
-	case <-time.After(3 * time.Minute):
-		clog.Errorf("SSH connection timeout exceeded")
+	case <-time.After(90 * time.Second):
+		clog.Debugf("SSH connection timeout exceeded 90 sec")
 		err = sshConn.Close()
 		if err != nil {
-			clog.Debugf("Error on SSH connection close: %s", err)
+			clog.Debugf("error on SSH connection close: %s", err)
 		}
 		return
 	}
@@ -262,16 +262,19 @@ func (cl *ClientListener) handleWebsocket(w http.ResponseWriter, req *http.Reque
 		failed(err)
 		return
 	}
+	clog.Debugf("client service started for %s", client.Name)
 
 	cl.replyConnectionSuccess(r, connRequest.Remotes)
 	cl.sendCapabilities(sshConn)
 
 	clientBanner := client.Banner()
-	clog.Debugf("Open %s", clientBanner)
+	clog.Debugf("open %s", clientBanner)
 	go cl.handleSSHRequests(clog, cid, reqs)
 	go cl.handleSSHChannels(clog, chans)
-	_ = sshConn.Wait()
-	clog.Debugf("Close %s", clientBanner)
+	if err = sshConn.Wait(); err != nil {
+		clog.Debugf("sshConn.Wait() error: %s", err)
+	}
+	clog.Debugf("close %s", clientBanner)
 
 	err = cl.clientService.Terminate(client)
 	if err != nil {
