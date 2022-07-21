cc65957b0696ec7463ca770b742916e57b194542
Thorsten Kramm <tk@system42.io>
2022-07-21 15:34:05 +0200

    Improved readability

--- share/logger/logger_test.go
+++ share/logger/logger_test.go
@@ -12,12 +12,14 @@ import (
 func TestLogger(t *testing.T) {
 	logfile := t.TempDir() + "/test.log"
 	l, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0444)
-	assert.NoError(t, err, "error creating log file")
+	require.NoError(t, err, "error creating log file")
 	defer l.Close()
+
 	logger := NewLogger("test", LogOutput{File: l}, LogLevelDebug)
 	logger.Debugf("Debug %s", "Debug")
 	logger.Infof("Info %s", "Info")
 	logger.Errorf("Error %s", "Error")
+
 	log, err := os.ReadFile(logfile)
 	require.NoError(t, err, "error reading log file")
 	assert.Contains(t, string(log), "test: Debug Debug")
