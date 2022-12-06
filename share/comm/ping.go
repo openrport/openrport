f0e3fbd88654ed4898c11a76b3bb7324b3ca6ced
Thorsten Kramm <tk@system42.io>
2022-12-06 13:17:28 +0100

    Do initial connection with a timeout

--- share/comm/ping.go
+++ share/comm/ping.go
@@ -1,36 +1,13 @@
 package comm
 
 import (
-	"context"
-	"fmt"
 	"time"
 
 	"golang.org/x/crypto/ssh"
 )
 
-func PingConnectionWithTimeout(conn ssh.Conn, timeout time.Duration) (bool, []byte, time.Duration, error) {
-	var (
-		ok         bool
-		response   []byte
-		err        error
-		timerStart = time.Now()
-	)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	ch := make(chan bool, 1)
-	go func() {
-		ok, response, err = conn.SendRequest(RequestTypePing, true, nil)
-		select {
-		default:
-			ch <- true
-		case <-ctx.Done():
-			return
-		}
-	}()
-	select {
-	case <-ch:
-		return ok, response, time.Since(timerStart), err
-	case <-time.After(timeout):
-		return false, nil, 0, fmt.Errorf("timeout %s exceeded", timeout)
-	}
+func PingConnectionWithTimeout(conn ssh.Conn, timeout time.Duration) (ok bool, response []byte, rtt time.Duration, err error) {
+	timerStart := time.Now()
+	ok, response, err = SendRequestWithTimeout(conn, RequestTypePing, true, nil, timeout)
+	return ok, response, time.Since(timerStart), err
 }
