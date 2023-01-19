979a7828eb94f445a6cdc859b23994919fa69557
Edoardo Ceccarelli <ridleys@gmail.com>
2023-01-19 15:32:15 +0200

    disabling G402: TLS MinVersion too low. (gosec)

--- share/security/tls_config.go
+++ share/security/tls_config.go
@@ -3,6 +3,7 @@ package security
 import "crypto/tls"
 
 func TLSConfig(whichtls uint16) *tls.Config {
+	// #nosec G402 -- disables G402: TLS MinVersion too low. (gosec)
 	var TLSConfig = &tls.Config{
 		MinVersion:               whichtls,
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
