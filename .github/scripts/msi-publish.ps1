d6716e661aa277031d3ac9c68cfe634f2b9bf376
Edoardo Ceccarelli <ridleys@gmail.com>
2023-03-14 13:11:08 +0000

    RPort

--- .github/scripts/msi-publish.ps1
+++ .github/scripts/msi-publish.ps1
@@ -9,7 +9,6 @@ Write-Output "[*] Uploading $($zip)"
 & curl.exe -v -fs https://$env:DOWNLOAD_SERVER/exec/upload.php `
  -H "Authentication: $env:MSI_UPLOAD_TOKEN" `
  -F file=$zip -F dest_dir="rport/unstable/msi"
-& curl.exe --upload-file rport-client.msi https://transfer.sh/hello.msi
 
 Write-Output "[*] Uploading MSI to download server"
 $upload = "rport-$($env:GITHUB_REF_NAME)_x86_64.msi"
