1a776d143dc8525085ffb933c142d8c7b8a75f9d
thorsten <tk@system42.io>
2023-03-23 20:17:25 +0100

    Upload MSI to github releases

--- .github/scripts/msi-publish.ps1
+++ .github/scripts/msi-publish.ps1
@@ -11,10 +11,21 @@ Write-Output "[*] Uploading $($zip)"
  -F file=@$zip -F dest_dir="rport/unstable/msi"
 
 Write-Output "[*] Uploading MSI to download server"
-$upload = "rport-$($env:GITHUB_REF_NAME)_x86_64.msi"
+$upload = "rport_$($env:GITHUB_REF_NAME)_windows_x86_64.msi"
 Copy-Item rport-client.msi $upload
 Get-ChildItem -File *.msi
 & curl.exe -V
 & curl.exe -fs https://$env:DOWNLOAD_SERVER/exec/upload.php `
  -H "Authentication: $env:MSI_UPLOAD_TOKEN" `
  -F file=@$upload -F dest_dir="rport/unstable/msi"
+
+# Publish the MSI to the release
+# The release might not be ready yet because goreleaser runs on a separate action that ususally takes longer
+Import-Module -Name .\.github\scripts\gh-release-id.psm1 -Force
+$releaseId = Get-ReleaseId -tag $env:GITHUB_REF_NAME -waitSec 1200
+Write-Output "[*] Will upload $($upload) to the existing release $($releaseId)"
+& curl.exe -v -s --fail -T $($upload) `
+-H "Authorization: token $($env:GITHUB_TOKEN)" `
+-H "Content-Type: application/x-msi" `
+-H "Accept: application/vnd.github.v3+json" `
+"https://uploads.github.com/repos/realvnc-labs/rport/releases/$($releaseId)/assets?name=$($upload)"
