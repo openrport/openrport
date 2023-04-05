d2777a82df416b658bbf783182a835724e4af51d
Edoardo Ceccarelli <ridleys@gmail.com>
2023-04-05 16:23:17 +0000

    msi upgrade final

--- .github/scripts/msi-test-install-upgrade.ps1
+++ .github/scripts/msi-test-install-upgrade.ps1
@@ -2,13 +2,6 @@ Write-Output "Install upgrade sequence msi test"
 Write-Output "---------------------------------"
 $ErrorActionPreference = 'Stop'
 
-Write-Output "[*] Installing goversioninfo"
-go install github.com/josephspurrier/goversioninfo/cmd/goversioninfo@latest
-$ENV:PATH="$ENV:PATH;$($env:home)\go\bin"
-
-Write-Output "[*] Installing WIX"
-choco install wixtoolset
-
 Write-Output "  - build rport msi ver. 0.1.2"
 .github/scripts/msi-buildver.ps1 0 1 2
 
@@ -19,9 +12,8 @@ if (Select-String -Path "msi-install0.1.2.log" -Pattern "The same or a newer ver
     Write-Error "install rport ver 0.1.2 failed: same or a newer version..."
 }
  
-Write-Output "  - editing rport.conf"
+Write-Output "  - adding and editing rport.conf"
 Add-Content "C:\Program Files\RPort\rport.conf" "`n# Hello, I was edited by the user in version 0.1.2"
-Get-Content "C:\Program Files\RPort\rport.conf" -Tail 3
 
 Write-Output "  - build rport msi ver. 1.3.4"
 .github/scripts/msi-buildver.ps1 1 3 4
@@ -41,7 +33,6 @@ if (-not($files.name.Contains('rport.conf')))
 }
 
 Write-Output "  - check rport.conf edited is the same "
-Get-Content "C:\Program Files\RPort\rport.conf" -Tail 3
 if (Select-String -Path "C:\Program Files\RPort\rport.conf" -Pattern "Hello, I was edited by the user in version 0.1.2" -SimpleMatch -Quiet)
 {
     Write-Output "  - found rport.conf edited content, all good!"
