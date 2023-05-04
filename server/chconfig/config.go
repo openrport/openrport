936e888e3bc7352705e1fc88e3224ace42ac9b76
domenipavec <domen.ipavec@gmail.com>
2023-05-04 18:08:09 +0000

    Add cors

--- server/chconfig/config.go
+++ server/chconfig/config.go
@@ -41,33 +41,34 @@ import (
 )
 
 type APIConfig struct {
-	Address                string  `mapstructure:"address"`
-	BaseURL                string  `mapstructure:"base_url"`
-	EnableAcme             bool    `mapstructure:"enable_acme"`
-	Auth                   string  `mapstructure:"auth"`
-	AuthFile               string  `mapstructure:"auth_file"`
-	AuthUserTable          string  `mapstructure:"auth_user_table"`
-	AuthGroupTable         string  `mapstructure:"auth_group_table"`
-	AuthGroupDetailsTable  string  `mapstructure:"auth_group_details_table"`
-	AuthHeader             string  `mapstructure:"auth_header"`
-	UserHeader             string  `mapstructure:"user_header"`
-	CreateMissingUsers     bool    `mapstructure:"create_missing_users"`
-	DefaultUserGroup       string  `mapstructure:"default_user_group"`
-	JWTSecret              string  `mapstructure:"jwt_secret"`
-	DocRoot                string  `mapstructure:"doc_root"`
-	CertFile               string  `mapstructure:"cert_file"`
-	KeyFile                string  `mapstructure:"key_file"`
-	AccessLogFile          string  `mapstructure:"access_log_file"`
-	UserLoginWait          float32 `mapstructure:"user_login_wait"`
-	MaxFailedLogin         int     `mapstructure:"max_failed_login"`
-	BanTime                int     `mapstructure:"ban_time"`
-	MaxTokenLifeTimeHours  int     `mapstructure:"max_token_lifetime"`
-	PasswordMinLength      int     `mapstructure:"password_min_length"`
-	PasswordZxcvbnMinscore int     `mapstructure:"password_zxcvbn_minscore"`
-	TLSMin                 string  `mapstructure:"tls_min"`
-	EnableWsTestEndpoints  bool    `mapstructure:"enable_ws_test_endpoints"`
-	MaxRequestBytes        int64   `mapstructure:"max_request_bytes"`
-	MaxFilePushSize        int64   `mapstructure:"max_filepush_size"`
+	Address                string   `mapstructure:"address"`
+	BaseURL                string   `mapstructure:"base_url"`
+	EnableAcme             bool     `mapstructure:"enable_acme"`
+	Auth                   string   `mapstructure:"auth"`
+	AuthFile               string   `mapstructure:"auth_file"`
+	AuthUserTable          string   `mapstructure:"auth_user_table"`
+	AuthGroupTable         string   `mapstructure:"auth_group_table"`
+	AuthGroupDetailsTable  string   `mapstructure:"auth_group_details_table"`
+	AuthHeader             string   `mapstructure:"auth_header"`
+	UserHeader             string   `mapstructure:"user_header"`
+	CreateMissingUsers     bool     `mapstructure:"create_missing_users"`
+	DefaultUserGroup       string   `mapstructure:"default_user_group"`
+	JWTSecret              string   `mapstructure:"jwt_secret"`
+	DocRoot                string   `mapstructure:"doc_root"`
+	CertFile               string   `mapstructure:"cert_file"`
+	KeyFile                string   `mapstructure:"key_file"`
+	AccessLogFile          string   `mapstructure:"access_log_file"`
+	UserLoginWait          float32  `mapstructure:"user_login_wait"`
+	MaxFailedLogin         int      `mapstructure:"max_failed_login"`
+	BanTime                int      `mapstructure:"ban_time"`
+	MaxTokenLifeTimeHours  int      `mapstructure:"max_token_lifetime"`
+	PasswordMinLength      int      `mapstructure:"password_min_length"`
+	PasswordZxcvbnMinscore int      `mapstructure:"password_zxcvbn_minscore"`
+	TLSMin                 string   `mapstructure:"tls_min"`
+	EnableWsTestEndpoints  bool     `mapstructure:"enable_ws_test_endpoints"`
+	MaxRequestBytes        int64    `mapstructure:"max_request_bytes"`
+	MaxFilePushSize        int64    `mapstructure:"max_filepush_size"`
+	CORS                   []string `mapstructure:"cors"`
 
 	TwoFATokenDelivery       string                 `mapstructure:"two_fa_token_delivery"`
 	TwoFATokenTTLSeconds     int                    `mapstructure:"two_fa_token_ttl_seconds"`
