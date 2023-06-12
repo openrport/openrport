4620347459a68735c6ff0673bf6440320127c73c
Jack Naisbett <jack.naisbett@gmail.com>
2023-06-12 13:50:17 +0100

    Make tls version configurable for caddy
    Adds support for tls_min parameter in caddy configuration to match option available for rportd

--- server/caddy/templates.go
+++ server/caddy/templates.go
@@ -55,7 +55,7 @@ const defaultVirtualHost = `
 {{ define "DVH"}}
 https://{{.ListenAddress}}:{{.ListenPort}} {
 	tls {{.CertsFile}} {{.KeyFile}} {
-		protocols tls1.3
+		protocols tls{{.TlsMin}}
 	}
 	respond "not found" 404
 }
@@ -66,7 +66,7 @@ const apiReverseProxySettingsTemplate = `
 {{ define "ARP"}}
 https://{{.ProxyDomain}}:{{.ProxyPort}} {
 	tls {{.CertsFile}} {{.KeyFile}} {
-		protocols tls1.3
+		protocols tls{{.TlsMin}}
 	}
 	reverse_proxy {
 		to {{.APIScheme}}://{{.APITargetHost}}:{{.APITargetPort}}
